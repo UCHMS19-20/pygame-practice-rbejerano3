a50522fa3feacd864db73e2b1de9968c17fd81e0
Ron <rbejerano@ucvts.org>
2019-12-12 10:32:00 -0500

    Epilepsy Warning

--- minimal.py
+++ minimal.py
@@ -7,6 +7,17 @@ pygame.init()
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 300) )
 
+#Colors
+red = pygame.Color(255, 0, 0)
+orange = pygame.Color(255, 128, 0)
+yellow = pygame.Color(255, 255, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+purple = pygame.Color(255, 0, 255)
+
+color_index = 0
+colors = [red, orange, yellow, green, blue, purple]
+
 # Main loop. Your game would go inside this loop
 while True:
     # do something for each event in the event queue (list of things that happen)
@@ -15,3 +26,10 @@ while True:
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+        while True:
+            color_index +=1
+            if color_index >= len(colors):
+                color_index = 0
+        screen.fill(colors[color_index])
+        pygame.display.flip()
+
